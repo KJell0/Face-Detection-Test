705312d70fa897c63b467ff30cc808a741f12b5d
KJell0 <158098237+KJell0@users.noreply.github.com>
2024-02-06 20:02:57 +0000

    Added comments to code, amended picture face detector to output an image.

--- liveFaceDetector.py
+++ liveFaceDetector.py
@@ -1,27 +1,38 @@
 import cv2
-
+#Trained xml file from Haarcascades (face and eyes)
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
+#Takes video from your webcam
 cap = cv2.VideoCapture(0)
 
+#Main loop to keep detecting
 while True:
+    #Read frames
     _, img = cap.read()
 
+    #Conversion to grayscale
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
+    # Detect face
     faces = face_cascade.detectMultiScale(gray, 1.2, 3)
 
+    #Drawing rectangles for the face
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
         roi_gray = gray[y:y + h, x:x + w]
         roi_color = img[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(roi_gray)
-        for (ex, ey, ew, eh) in eyes:
-            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
+    #Detect eyes
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+
+    #Drawing rectangles for the eyes
+    for (ex, ey, ew, eh) in eyes:
+        cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+    #Display
     cv2.imshow('img', img)
 
+    #Exit with ESC
     k = cv2.waitKey(30) & 0xff
     if k==27:
         break
--- pictureFaceDetector.py
+++ pictureFaceDetector.py
@@ -1,12 +1,16 @@
 import cv2
-
+#trained xml file for face detection
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-
-img = cv2.imread('test.jpg')
-
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#input image
+image = cv2.imread('test.jpg')
+#convert image into greyscale for detection
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-
+#rectangle for faces
 for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+#display
+cv2.imshow('img', image)
+cv2.waitKey()
